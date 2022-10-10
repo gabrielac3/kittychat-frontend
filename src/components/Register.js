70875f33e6af5432f8302e82ee11009d56efd3a4
day all <danielafuentesdev@gmail.com>
2022-10-10 16:00:56 -0500

    adding error modal to register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 
-export const Register= () => {
-  const [resMsg, setErrorMsg] = React.useState('')
+export const Register= (props) => {
+  // const [resMsg, setErrorMsg] = React.useState('')
 
   const addUser = () => {
     return fetch('http://localhost:3100/addUser', {
@@ -44,17 +44,16 @@ export const Register= () => {
     if(indexOfEqual === undefined || indexOfEqual === -1) return
     const msgError = resMsg.slice(0, indexOfEqual)
     if(msgError === 'Key (user_name)'){
-      setErrorMsg('Username already in use')
+      props.onErrorMsg('Username already in use')
     } else if (msgError === 'Key (email)'){
-      setErrorMsg('Email already in use')
-    } else setErrorMsg('else')
+      props.onErrorMsg('Email already in use')
+    } else props.onErrorMsg('else')
   }
 
   return (
     <form className='register' onSubmit={handleSubmit}>
         <div className='form'>
             <p>Welcome</p>
-            <p>{resMsg}</p>
             <h1>Register your account</h1>
             <label>User</label>
             <input
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,6 +4,12 @@ import { Login } from './Login'
 import { Register } from './Register'
 
 export const Welcome = () => {
+  const [errorMsg, setErrorMsg] = React.useState('')
+/*   handleChange = (event) => {
+    this. setState({
+        chosen: event. target. value
+   })
+ */
   return (
     <div className='welcome-main'>
       <div className='mountain'>
@@ -14,11 +20,12 @@ export const Welcome = () => {
           <img src='../image/logoCat.png' className="img-logo" alt='logo-cat' />
         </div>
         <Routes>
-          <Route path='/' element = {<Login />}></Route>
-          <Route path='/login' element = {<Login />}></Route>
-          <Route path='/register' element = {<Register />}></Route>
+          <Route path='/' element = {<Login onErrorMsg = {setErrorMsg} />}></Route>
+          <Route path='/login' element = {<Login onErrorMsg = {setErrorMsg} />}></Route>
+          <Route path='/register' element = {<Register onErrorMsg = { str => setErrorMsg(str)} />}></Route>
         </Routes>
       </div>
+      <div>{errorMsg}</div>
     </div>
   )
 }
