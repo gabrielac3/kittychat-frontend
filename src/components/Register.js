a19a9c241975dc6846a0ecae03316caa9b4bb1ec
Paola <glenypaola.7@gmail.com>
2022-11-01 22:13:06 +0100

    none

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,92 +1,97 @@
-import React from 'react'
+import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-export const Register= (props) => {
+export const Register = (props) => {
   const navigate = useNavigate();
   // const [resMsg, setErrorMsg] = React.useState('')
 
   const addUser = () => {
-    return fetch('http://localhost:3100/addUser', {
-      method: 'POST',
+    return fetch("http://localhost:3100/addUser", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(formData)
+      body: JSON.stringify(formData),
     })
-    .then(res => {
-      if(!res.ok) return res.json()
-      else {
-        navigate('/login');  
-        return res.json();
-      }
-    })
-    .catch(error => console.log(error))
-  }
+      .then((res) => {
+        if (!res.ok) return res.json();
+        else {
+          navigate("/login");
+          return res.json();
+        }
+      })
+      .catch((error) => console.log(error));
+  };
 
-  const [formData, setFormData] = React.useState(
-    {nameUser: "", email: "", password: ""}
-  )
+  const [formData, setFormData] = React.useState({
+    nameUser: "",
+    email: "",
+    password: "",
+  });
 
   function handleChange(event) {
-    setFormData(prevFormData => {
-        return {
-            ...prevFormData,
-            [event.target.name]: event.target.value
-        }
-    })
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [event.target.name]: event.target.value,
+      };
+    });
   }
 
-  async function handleSubmit (e) {
+  async function handleSubmit(e) {
     e.preventDefault();
     const res = await addUser();
-    showError(res)
+    showError(res);
   }
 
   const showError = (res) => {
-    const resMsg = res.message
-    const indexOfEqual = resMsg.indexOf('=') 
-    if(indexOfEqual === undefined || indexOfEqual === -1) return
-    const msgError = resMsg.slice(0, indexOfEqual)
-    if(msgError === 'Key (user_name)'){
-      props.onErrorMsg('Username already in use')
-    } else if (msgError === 'Key (email)'){
-      props.onErrorMsg('Email already in use')
-    } else props.onErrorMsg('else')
-  }
+    const resMsg = res.message;
+    const indexOfEqual = resMsg.indexOf("=");
+    if (indexOfEqual === undefined || indexOfEqual === -1) return;
+    const msgError = resMsg.slice(0, indexOfEqual);
+    if (msgError === "Key (user_name)") {
+      props.onErrorMsg("Username already in use");
+    } else if (msgError === "Key (email)") {
+      props.onErrorMsg("Email already in use");
+    } else props.onErrorMsg("else");
+  };
 
   return (
-    <form className='register' onSubmit={handleSubmit}>
-        <div className='form'>
-            <p>Welcome</p>
-            <h1>Register your account</h1>
-            <label>User</label>
-            <input
-             type='text'
-             name="nameUser"
-             value={formData.nameUser}
-             onChange={handleChange}
-            ></input>
+    <form className="register" onSubmit={handleSubmit}>
+      <div className="form">
+        <p>Welcome</p>
+        <h1>Register your account</h1>
+        <label>User</label>
+        <input
+          type="text"
+          name="nameUser"
+          value={formData.nameUser}
+          onChange={handleChange}
+        ></input>
 
-            <label>Email</label>
-            <input 
-              type='email'
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-            ></input>
+        <label>Email</label>
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        ></input>
 
-            <label>Password</label>
-            <input 
-              type='password'
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-            ></input>
-            <button type='submit' >Sign Up</button>
-        </div>
-        <p>Already have an account?
-          <Link to="/login" className='anchor'>Sign in</Link>
-        </p>
-    </form>  
-  )
-}
+        <label>Password</label>
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        ></input>
+        <button type="submit">Sign Up</button>
+      </div>
+      <p>
+        Already have an account?
+        <Link to="/login" className="anchor">
+          Sign in
+        </Link>
+      </p>
+    </form>
+  );
+};
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,26 +1,35 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import { Login } from './Login'
-import { Register } from './Register'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Login } from "./Login";
+import { Register } from "./Register";
 
 export const Welcome = () => {
-  const [errorMsg, setErrorMsg] = React.useState('')
+  const [errorMsg, setErrorMsg] = React.useState("");
   return (
-    <div className='welcome-main'>
-      <div className='mountain'>
-        <img src='../image/mountain.jpg' alt='background' />
+    <div className="welcome-main">
+      <div className="mountain">
+        <img src="../image/mountain.jpg" alt="background" />
       </div>
-      <div className='halfPurple'>
-        <div className='logo-container'>
-          <img src='../image/logoCat.png' className="img-logo" alt='logo-cat' />
+      <div className="halfPurple">
+        <div className="logo-container">
+          <img src="../image/logoCat.png" className="img-logo" alt="logo-cat" />
         </div>
         <Routes>
-          <Route path='/' element = {<Login onErrorMsg = {str => setErrorMsg(str)} />}></Route>
-          <Route path='/login' element = {<Login onErrorMsg = {str => setErrorMsg(str)} />}></Route>
-          <Route path='/register' element = {<Register onErrorMsg = { str => setErrorMsg(str)} />}></Route>
+          <Route
+            path="/"
+            element={<Login onErrorMsg={(str) => setErrorMsg(str)} />}
+          ></Route>
+          <Route
+            path="/login"
+            element={<Login onErrorMsg={(str) => setErrorMsg(str)} />}
+          ></Route>
+          <Route
+            path="/register"
+            element={<Register onErrorMsg={(str) => setErrorMsg(str)} />}
+          ></Route>
         </Routes>
       </div>
       <div>{errorMsg}</div>
     </div>
-  )
-}
+  );
+};
