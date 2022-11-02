1575c5344f7b5e93055c78484ff172be6f63349c
GabrielaC3 <Gabycr.3.2.gcr@gmail.com>
2022-11-02 11:04:02 -0500

    choose color

--- src/components/ModalChooseColor.js
+++ src/components/ModalChooseColor.js
@@ -1,40 +1,35 @@
 import React from 'react';
 import axios from 'axios';
-
 const colorProps = [
     {
       id: 'color1',
       src: 'https://www.pngitem.com/pimgs/m/518-5182033_streamlabs-chatbot-hd-png-download.png',
-      color: "#646FD0"
+      hexName: "#646FD0"
     },
     {
       id: 'color2',
       src: 'https://img2.freepng.es/20180324/cue/kisspng-open-broadcaster-software-streaming-media-streamla-donate-5ab66113098343.960710561521901843039.jpg',
-      color: "#245CAC"
+      hexName: "#245CAC"
     },
     {
       id: 'color3',
       src: 'https://img2.freepng.es/20180719/lpy/kisspng-streamlabs-mixer-streaming-media-open-broadcaster-avatar-discord-5b514c143c3f67.4769472415320545482468.jpg',
-      color: "#549DCF"
+      hexName: "#549DCF"
     }
 ]
-
 export const ModalChooseColor = (props) => {
-    async function changeColor(avatar) {
-        try {
-          const response = await axios.post('http://localhost:3100/updateColor', {
-            email: props.userSession.email,
-            avatarColor: avatar.color
-          });
-          // setAvatar(avatar.src)
-/*           setAvatarChange(avatar.src) */
-          props.toggleModal(['chooseColor'])
-        } catch (error) {
-          console.error(error.message);
-        }
+  async function changeColor(color) {
+      try {
+        const response = await axios.post('http://localhost:3100/updateColor', {
+          email: props.userSession.email,
+          avatarColor: color.hexName
+        });
+        props.setColor(color.hexName)
+        props.toggleModal(['chooseColor'])
+      } catch (error) {
+        console.error(error.message);
       }
-
-
+  }
   return (
     <div className='modal-shadow-bg m-choose-color'>
       <div className='modal m-join-to-channel'>
@@ -43,10 +38,10 @@ export const ModalChooseColor = (props) => {
           <i class="fa-solid fa-xmark"></i>
         </span>
         <div className='m-choose-colors flex'>
-          {colorProps.map(color => 
-            <div 
-              key={color.id} 
-              className = {`c-${color.color.slice(1)}`}
+          {colorProps.map(color =>
+            <div
+              key={color.id}
+              className = {`c-${color.hexName.slice(1)}`}
               onClick={()=> changeColor(color)}>
             </div>
           )}
